833b3f5781ec22b199de8affe937d33ca012c824
Gabor Szabo <gabor@szabgab.com>
2020-10-04 00:32:58 +0300

    -s

--- lib/App/Deparse/Tk.pm
+++ lib/App/Deparse/Tk.pm
@@ -3,16 +3,20 @@ use strict;
 use warnings;
 use 5.008;
 
+use Browser::Open qw(open_browser open_browser_cmd);
 use Path::Tiny qw(path);
 
 use Tk;
+use Tk::Dialog;
+use Tk::HyperText;
+use Tk::BrowseEntry;
 
 our $VERSION = '0.01';
 
-# TODO make fonts more readable
-# TODO make it clear what is the input window and what is the output window
-# TODO Clear the output when we change the input (or maybe rerun the deparse process?)
-# TODO make the output window read-only
+# TODO: make fonts more readable
+# TODO: Clear the output when we change the input (or maybe rerun the deparse process?)
+# TODO: use nice temporary filename as we can see the name of the file with the -l flags
+# TODO: Save window size upon exit; restore window size upon start
 
 my $sample = q{
 # Paste your code in the top window and click the Deparse button to see what B::Deparse thinks about it
@@ -30,13 +34,113 @@ sub new {
     $self->{top} = MainWindow->new(
         -title => 'B::Deparse',
     );
-    #$self->create_menu;
+    $self->create_menu;
     $self->create_app;
     $self->{incode}->insert("0.0", $sample);
     $self->deparse;
 
     return $self;
 }
+
+sub create_menu {
+    my ($self) = @_;
+
+    my $main_menu = $self->{top}->Menu();
+
+    my $file_menu = $main_menu->cascade(-label => 'File', -underline => 0);
+    #$file_menu->command(-label => 'Open Perl File', -command => sub { $self->show_open(); }, -underline => 0);
+    $file_menu->command(-label => 'Quit (Ctrl-q)', -command => sub { $self->exit_app(); }, -underline => 0);
+
+    my $about_menu = $main_menu->cascade(-label => 'Help', -underline => 0);
+    $about_menu->command(-label => 'About', -command => sub { $self->show_about; }, -underline => 0);
+
+    $self->{top}->configure(-menu => $main_menu);
+}
+
+sub show_about {
+    my ($self) = @_;
+
+    my $dialog = $self->{top}->DialogBox(
+        -title   => 'About App::Deparse::Tk',
+        -popover => $self->{top},
+        -buttons => ['OK'],
+    );
+
+    my $html = $dialog->HyperText();
+    $html->pack;
+    $html->setHandler (Resource => \&onResource);
+    $html->loadString(qq{<html>
+      <head>
+      <title>About App::Deparse::Tk</title>
+      </head>
+      <body>
+         Version: $VERSION<br>
+         &nbsp;<a href="https://metacpan.org/pod/Tk">Perl Tk</a>: $Tk::VERSION<br>
+         <a href="https://metacpan.org/pod/B::Deparse">B::Deparse</a><br>
+         Perl $]<br>
+         <p>
+         Create by Gabor Szabo<br>
+         Source code on <a href="https://github.com/szabgab/App-PerlTidy-Tk">GitHub</a><br>
+         Thanks to my <a href="https://www.patreon.com/szabgab">Patreon</a> supporters<br>
+
+<h2>Supporters</h2>
+<ul>
+<li><a href="https://www.activestate.com/">ActiveState</a></li>
+<li><a href="https://www.chatterjee.net/">Anirvan Chatterjee</a></li>
+<li>Brian Gaboury</li>
+<li><a href="https://www.linkedin.com/in/chan-wilson-b867b3/">Chan Wilson</a></li>
+<li><a href="https://www.linkedin.com/in/fins0ck/">Csaba Gaspar</a></li>
+<li><a href="https://www.linkedin.com/in/dihnen/">David Ihnen</a></li>
+<li><a href="https://www.preshweb.co.uk/about/">David Precious</a></li>
+<li>Frank Kropp</li>
+<li>John Andersen</li>
+<li>Magnus Enger</li>
+<li><a href="https://www.linkedin.com/in/mjgardner/">Mark Gardner</a></li>
+<li>Markus Hechenberger</li>
+<li>Matthew Mitchell</li>
+<li>Matthew Persico</li>
+<li>Meir Guttman</li>
+<li>Mike Small</li>
+<li><a href="https://www.linkedin.com/in/n8dgr8/">Nathan Schlehlein</a></li>
+<li><a href="https://www.linkedin.com/in/pfmabry/">Paul Mabry</a></li>
+<li><a href="https://perl.careers/">Perl Careers</a></li>
+<li>Richard Leach</li>
+<li>Robert Coursen</li>
+<li><a href="https://www.linkedin.com/in/shajiindia/">Shaji Kalidasan</a></li>
+<li><a href="https://www.linkedin.com/in/shanta-mcbain-7b644437/">Shanta McBain</a></li>
+<li><a href="https://www.linkedin.com/in/slobodanmiskovic/">Slobodan Mišković</a></li>
+<li><a href="https://www.linkedin.com/in/stephen-jarjoura-a684401/">Stephen A. Jarjoura</a></li>
+<li>Tony Edwardson</li>
+<li><a href="https://www.linkedin.com/in/tori-hunter-00009639/">Tori Hunter</a></li>
+<li><a href="https://bruck.co.il/">Uri Bruck</a></li>
+<li>Victor Moral</li>
+<li>Wolfgang Odendahl</li>
+<li>Yes2Crypto</li>
+<li>... + many others</li>
+</ul>
+
+      </body>
+      </html>
+    });
+
+    $dialog->Show;
+}
+
+sub onResource {
+    my ($html, %info) = @_;
+    my $url = $info{href};
+    #print $url, "\n";
+    #open_browser($url); # https://rt.cpan.org/Public/Bug/Display.html?id=133315
+    #print "done\n";
+    my $cmd = open_browser_cmd($url);
+    # TODO: verify that the URL is well formatted before passing it to system
+    if ($^O eq 'MSWin32') {
+        system("$cmd $url");
+    } else {
+        system("$cmd $url &");
+    }
+}
+
 
 sub create_app {
     my ($self) = @_;
@@ -49,6 +153,7 @@ sub create_app {
 
     $self->{flags} = $self->{top}->Frame();
     $self->{flags}->pack(-side => 'top');
+
     for my $flag (@flags) {
         $self->{"${flag}_flag"} = 0;
 
@@ -60,9 +165,20 @@ sub create_app {
         );
         $self->{"${flag}_flag_checkbox"}->pack(-side => 'left');
     }
+    $self->{"s_flag"} = '';
+    my @s_values = qw(C i T);
+    $self->{"s_flag_widget"} = $self->{flags}->BrowseEntry(
+        -label => "-s",
+        -width => 3,
+        -variable => \$self->{"s_flag"},
+    );
+    for my $s (@s_values) {
+        $self->{"s_flag_widget"}->insert("end", $s);
+    }
+    $self->{"s_flag_widget"}->pack;
 
     $self->{outcode} = $self->{top}->Text(
-        -state => 'normal',
+        -state => 'disabled',
         -font => ['fixed', 12],
     );
     $self->{outcode}->pack(-fill => 'both', -expand => 1);
@@ -78,33 +194,47 @@ sub create_app {
 
 sub deparse {
     my ($self) = @_;
+
     my $code = $self->{incode}->get("0.0", 'end');
     my $temp = Path::Tiny->tempfile;
     path($temp)->spew($code);
-    # TODO: handle STDERR
-    # TODO: handle exit code
         
     my $cmd = 'perl -MO=Deparse';
-    if ($self->{d_flag}) {
-        $cmd .= ',-d'
+    for my $flag (@flags) {
+        if ($self->{"${flag}_flag"}) {
+            $cmd .= ",-$flag";
+        }
     }
-    if ($self->{p_flag}) {
-        $cmd .= ',-p'
-    }
-    if ($self->{q_flag}) {
-        $cmd .= ',-q'
+    if ($self->{s_flag}) {
+        $cmd .= "-s$self->{s_flag}"
     }
 
+    # TODO: handle STDERR
+    # TODO: handle exit code
     my $out = qx{$cmd $temp};
-    # TODO: save to file, run deparse on it, get the output and paste it in the same window (or maybe another window?)
+    $self->{outcode}->configure('state' => 'normal');
     $self->{outcode}->delete("0.0", 'end');
     $self->{outcode}->insert("0.0", $out);
-
+    $self->{outcode}->configure('state' => 'disabled');
 }
 
 sub run {
     my ($self) = @_;
     MainLoop;
+}
+
+sub exit_app {
+    my ($self) = @_;
+
+    # TODO: Save flags when exiting
+    # TODO: Save content of the input window when exiting
+    # TODO: Save current window size so we can start the same size next time.
+    # my %config = (
+    #     'geometry' => $self->{top}->geometry,
+    # );
+    # path($config_file)->spew(encode_json(\%config));
+
+    exit;
 }
 
 1;
